2057b8fcdfed1be7217c29b06fa0f1e955cdadcc
Sreedeepta <78677605+sreedeepta@users.noreply.github.com>
2023-03-28 16:07:15 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,11 +1,11 @@
 class area
 { 
-  public static void main (string a , string b)
-  { 
-   string area = a*b;
-   string radius = b/2;
-   string areaofsemicircle = 1/2(radius * radius * 22/7);
-   string areatotal = area + areaofsemicircle;
-   System.out.println("Total Area : " + areatotal);
-   }
+  public static void main (int x , int y)
+  {
+    int area = x * y;
+    int radius = y/2;
+    int areasemicircle = 1/2*(radius*radius*22/7);
+    int totalarea = area + areasemicircle;
+    System.out.println("Total Area = " + areasemicircle);
+  }
 }
